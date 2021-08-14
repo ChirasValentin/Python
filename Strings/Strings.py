0856d6ff9227596a79c31d568db05b91a1417eb0
ChirasValentin <56472147+ChirasValentin@users.noreply.github.com>
2021-08-14 14:15:02 +0300

    strings in python

---
+++ Strings/Strings.py
@@ -0,0 +1,21 @@
+course = "Python's Course for Beginners"
+print(course)
+course = 'Python for "Beginners"'
+print(course)
+course ='''
+ Hi John,
+ 
+ First email.
+ 
+ All regards,
+ Support team!\n'''
+print(course)
+
+course = 'Python for Beginners'
+print(course)
+print('course[0] = ' + course[0])
+print('course[-1] = ' + course[-1])
+print('course[0:3] = ' + course[0:3])
+print('course[0:] = ' + course[0:])
+print('course[2:] = ' + course[2:])
+print('course[:5] = ' + course[:5])
---
+++ Type conversion/conversion.py
@@ -0,0 +1,12 @@
+# birth_year = input('Birth year: ')
+# age = 2021 - birth_year  # TypeError: unsupported operand type(s) for -: 'int' and 'str'
+# print(type(birth_year))
+# age = 2021 - int(birth_year)
+# print(type(age))
+# print(age)
+
+# exercise
+pounds = input ("What is your weight (in pounds) ")
+kilograms = float(pounds)*0.45
+print("You have " + str(kilograms) + ' kg ')
+
