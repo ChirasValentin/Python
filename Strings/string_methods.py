775dea8b817c17ac60248c94d4962c23065d5a8d
ChirasValentin <56472147+ChirasValentin@users.noreply.github.com>
2021-08-14 14:41:30 +0300

    add gitgore and new files

---
+++ Strings/string_methods.py
@@ -0,0 +1,10 @@
+course = 'Python for Beginners'
+print(len(course))
+print(course.upper())
+print(course)
+print(course.find('o'))
+print(course.find('O'))
+print(course.find('Beginners'))
+print(course.replace('Beginners', "Absolute Beginners"))
+print('Python' in course)
+print('python' in course)
