41fb96c0b2d2faa8d513c8f3323a8f4a26f5edb9
ChirasValentin <56472147+ChirasValentin@users.noreply.github.com>
2021-08-15 20:41:00 +0300

    lists

---
+++ Lists/lists.py
@@ -0,0 +1,9 @@
+names = ['John', 'Bob', 'Mosh', 'Sarah', 'Mike']
+print(names)
+print(names[0])
+print(names[-1])
+print(names[2:])
+print(names[2:4])
+print(names[0:-1])
+names[0] = 'Jon'
+print(names)
