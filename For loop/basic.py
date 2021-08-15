feb7b3d657e2628bc91b3486167577e86a32687a
ChirasValentin <56472147+ChirasValentin@users.noreply.github.com>
2021-08-15 20:28:51 +0300

    for loop and nested loops

---
+++ For loop/basic.py
@@ -0,0 +1,18 @@
+print("for item in 'Pyhon' result: ")
+for item in 'Python':
+    print(item)
+print(f"for item in [Mosh, Jhon, Sarah] result: ")
+for item in ['Mosh', 'Jhon', 'Sarah']:
+    print(item)
+print(f"for item in [1, 2, 3, 4] result: ")
+for item in [1, 2, 3, 4]:
+    print(item)
+print(f"for item in range(10) result: ")
+for item in range(10):
+    print(item)
+print(f"for item in range(5,10) result: ")
+for item in range(5, 10):
+    print(item)
+print(f"for item in range(5, 10, 2) result: ")
+for item in range(5, 10, 2):
+    print(item)
