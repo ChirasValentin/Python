7598c5518f58edd1df8d4a2217308ad9df0a14f2
ChirasValentin <56472147+ChirasValentin@users.noreply.github.com>
2021-08-30 21:15:12 +0300

    Learned Dictionary, Tuples, emoji converter,  unpacking

---
+++ Tuples/tuples.py
@@ -0,0 +1,4 @@
+numbers = [1,2,3]
+numbersAsTuple=(1,2,3)
+## numbers[0] = 10 -> error
+print(numbersAsTuple[0])
---
+++ Tuples/unpacking.py
@@ -0,0 +1,7 @@
+coordinates = (1, 2, 3)
+print(coordinates[0] * coordinates[1] * coordinates[2])
+x, y, z = coordinates
+## same as x= coordinates[0]
+##         y = coordinates[1]
+##         x =coordinates[2]
+print(x, y, z)
